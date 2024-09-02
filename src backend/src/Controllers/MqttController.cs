ab15991671dfe0204149d2910aeb719b20848f3c
Muzzammil Imran <m.muzzammil.imran@gmail.com>
2024-09-02 18:32:15 +0500

    Code cleaned

--- src backend/src/Controllers/MqttController.cs
+++ src backend/src/Controllers/MqttController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MQTTnet.Server;
+using System.Text;
 
 namespace src.Controllers
 {
     public class MqttController : Controller
     {
-        public IActionResult Index()
+
+        public MqttController()
         {
-            return View();
+        }
+
+        public Task OnClientConnected(ClientConnectedEventArgs eventArgs)
+        {
+            Console.WriteLine($"Client '{eventArgs.ClientId}' connected.");
+            return Task.CompletedTask;
+        }
+
+        public Task ValidateConnection(ValidatingConnectionEventArgs eventArgs)
+        {
+            Console.WriteLine($"Client '{eventArgs.ClientId}' wants to connect. Accepting!");
+            return Task.CompletedTask;
+        }
+
+        // This method is triggered when the server receives a message from a client.
+        public Task OnMessageReceived(InterceptingPublishEventArgs eventArgs)
+        {
+            var payload = eventArgs.ApplicationMessage.Payload;
+            var message = Encoding.UTF8.GetString(payload);
+
+            Console.WriteLine($"Message received on topic '{eventArgs.ApplicationMessage.Topic}': {message}");
+            return Task.CompletedTask;
         }
     }
 }
---
+++ src backend/src/Startup.cs
@@ -0,0 +1,42 @@
+﻿using MQTTnet.AspNetCore;
+using src.Controllers;
+
+namespace src
+{
+    public class StartUp
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment, MqttController mqttController)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapConnectionHandler<MqttConnectionHandler>("/mqtt", httpConnectionDispatcherOptions =>
+                {
+                    httpConnectionDispatcherOptions.WebSockets.SubProtocolSelector =
+                        protocolList => protocolList.FirstOrDefault() ?? string.Empty;
+                });
+            });
+
+            app.UseMqttServer(server =>
+            {
+                server.ValidatingConnectionAsync += mqttController.ValidateConnection;
+                server.ClientConnectedAsync += mqttController.OnClientConnected;
+                server.InterceptingPublishAsync += mqttController.OnMessageReceived;
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHostedMqttServer(optionsBuilder =>
+            {
+                optionsBuilder.WithDefaultEndpoint();
+            });
+
+            services.AddMqttConnectionHandler();
+            services.AddConnections();
+
+            services.AddSingleton<MqttController>();
+        }
+    }
+}
