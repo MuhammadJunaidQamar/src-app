ffd2b624724ec85d671078583cdd9516ff4a00ef
Muhammad Junaid Qamar <muhammadjunaidqamar@gmail.com>
2025-03-04 22:50:13 +0500

    updated dependencies and added

--- src frontend/src/lib/widgets/live_camera_feed_widget.dart
+++ src frontend/src/lib/widgets/live_camera_feed_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mjpeg_stream/mjpeg_stream.dart';
 
 class LiveCameraFeedWidget extends StatelessWidget {
   const LiveCameraFeedWidget({super.key});
@@ -7,6 +8,14 @@ class LiveCameraFeedWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     var w = MediaQuery.of(context).size.width;
 
-    return Center(child: Text('Live Camera Feed$w'));
+    return Center(
+      child: MJPEGStreamScreen(
+        width: w,
+        height: w * 9 / 16,
+        streamUrl: 'http://',
+        showLiveIcon: true,
+        watermarkText: 'UCP',
+      ),
+    );
   }
 }
